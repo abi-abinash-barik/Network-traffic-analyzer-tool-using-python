5540a2106c2bcca787a1b0e8fab6f224c320953f
abi-abinash-barik <abi.abinash.barik@gmail.com>
2024-07-20 22:46:03 +0530

    Add files via upload

---
+++ visualization/traffic_visualization.py
@@ -0,0 +1,7 @@
+# visualization/traffic_visualization.py
+import matplotlib.pyplot as plt
+
+def visualize_traffic(data):
+    # Create graphs or charts based on the data
+    print("Visualizing traffic...")
+    # Add more visualization logic as needed
